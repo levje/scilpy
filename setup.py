acb2c7b3a81e4557aa11b547346ddeed2d1969a4
Jeremi Levesque <jeremilevesque@hotmail.com>
2025-07-14 15:29:32 -0400

    First steps at removing setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,7 @@
 import os
-
+import glob
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
-
-with open('requirements.txt') as f:
-    required_dependencies = f.read().splitlines()
-    external_dependencies = []
-    for dependency in required_dependencies:
-        if dependency[0:2] == '-e':
-            repo_name = dependency.split('=')[-1]
-            repo_url = dependency[3:]
-            external_dependencies.append('{} @ {}'.format(repo_name, repo_url))
-        else:
-            external_dependencies.append(dependency)
-
 
 def get_extensions():
     define_macros = [('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')]
@@ -49,17 +37,10 @@ class CustomBuildExtCommand(build_ext):
         build_ext.finalize_options(self)
         build_ext.run(self)
 
-
-# Get the requiered python version
-PYTHON_VERSION = ""
-with open('.python-version') as f:
-    f.readline()
-    PYTHON_VERSION = f.readline().strip("\n")
-
-# Get version and release info, which is all stored in scilpy/version.py
-ver_file = os.path.join('scilpy', 'version.py')
-with open(ver_file) as f:
-    exec(f.read())
+LEGACY_SCRIPTS = filter(lambda s: not os.path.basename(s) == "__init__.py",
+                        glob.glob("scripts/legacy/*.py"))
+SCRIPTS = filter(lambda s: not os.path.basename(s) == "__init__.py",
+                 glob.glob("scripts/*.py"))
 
 entry_point_legacy = []
 if os.getenv('SCILPY_LEGACY') != 'False':
@@ -67,37 +48,20 @@ if os.getenv('SCILPY_LEGACY') != 'False':
                           os.path.basename(s),
                           os.path.basename(s).split(".")[0]) for s in LEGACY_SCRIPTS]
 
-opts = dict(name=NAME,
-            maintainer=MAINTAINER,
-            maintainer_email=MAINTAINER_EMAIL,
-            description=DESCRIPTION,
-            long_description=LONG_DESCRIPTION,
-            url=URL,
-            download_url=DOWNLOAD_URL,
-            license=LICENSE,
-            classifiers=CLASSIFIERS,
-            author=AUTHOR,
-            author_email=AUTHOR_EMAIL,
-            platforms=PLATFORMS,
-            version=VERSION,
-            packages=find_packages(),
-            cmdclass={
-                'build_ext': CustomBuildExtCommand
-            },
-            ext_modules=get_extensions(),
-            python_requires=PYTHON_VERSION,
-            setup_requires=[],  # replaced by PEP518 (pyproject.toml)
-            install_requires=external_dependencies,
-            entry_points={
-                'console_scripts': ["{}=scripts.{}:main".format(
-                    os.path.basename(s),
-                    os.path.basename(s).split(".")[0]) for s in SCRIPTS] +
-                entry_point_legacy
-            },
-            data_files=[('data/LUT',
-                         ["data/LUT/freesurfer_desikan_killiany.json",
-                          "data/LUT/freesurfer_subcortical.json",
-                          "data/LUT/dk_aggregate_structures.json"])],
-            include_package_data=True)
+entry_point = ["{}=scripts.{}:main".format(
+                  os.path.basename(s),
+                  os.path.basename(s).split(".")[0]) for s in SCRIPTS]
 
-setup(**opts)
+
+setup(packages=find_packages(),
+      cmdclass={'build_ext': CustomBuildExtCommand},
+      ext_modules=get_extensions(),
+      scripts=entry_point + entry_point_legacy,
+    #   entry_points={
+    #       'console_scripts': entry_point + entry_point_legacy
+    #   },
+      data_files=[('data/LUT',
+                   ["data/LUT/freesurfer_desikan_killiany.json",
+                    "data/LUT/freesurfer_subcortical.json",
+                    "data/LUT/dk_aggregate_structures.json"])],
+      include_package_data=True)
